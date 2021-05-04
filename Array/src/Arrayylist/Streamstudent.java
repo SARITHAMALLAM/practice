4ba657d8916a6062841a0f3d2ff263ca9bf20800
sarithalatish@gmail.com <sarithalatish@gmail.com>
2021-05-04 17:30:48 -0500

    first one

---
+++ Array/src/Arrayylist/Streamstudent.java
@@ -0,0 +1,14 @@
+package Arrayylist;
+
+public interface Streamstudent {
+
+	
+	.sorted((x) -> x.getLastName().
+			.filter( y -> vowels.cotains(y.getFirstNam().charAt(0)));
+			.forEach(y -> System.out.println(y.getFirstName() + x.getLastName));
+			
+			students.stream().map(y -> y.getFirstName()).forEach (name -> System.out.println(name));
+
+		}
+
+}
---
+++ Array/src/Arrayylist/name.java
@@ -0,0 +1,5 @@
+package Arrayylist;
+
+public interface name {
+
+}
